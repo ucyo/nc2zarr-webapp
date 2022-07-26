77321851920fcc6e1c286653b1efe6d1cdd3ef9f
Stefan Bechert <stefan.bechert.inf@gmail.com>
2022-07-26 19:32:29 +0200

    [TEST] Replace paths for HTTP access.

--- backend/worker/intake_catalog_generation/intake_catalog_generator.py
+++ backend/worker/intake_catalog_generation/intake_catalog_generator.py
@@ -4,9 +4,9 @@ import intake
 import requests
 
 
-def generate_intake_catalog_for_zarr(relative_input_path: str, file_name: str, complete_conversion_id: int):
-    relative_input_path = build_relative_input_path(relative_input_path)
-    path = os.path.join(relative_input_path, file_name + ".zarr")
+def generate_intake_catalog_for_zarr(absolute_input_path: str, file_name: str, complete_conversion_id: int):
+    absolute_input_path = build_input_path(absolute_input_path)
+    path = os.path.join(absolute_input_path, file_name + ".zarr")
 
     source = intake.open_zarr(path, decode_times=False)
     source.discover()
@@ -16,19 +16,21 @@ def generate_intake_catalog_for_zarr(relative_input_path: str, file_name: str, c
     if os.environ["NC2ZARR_PROD"] == "True":
         domain = "django:8000"
 
+    yaml = source.yaml().replace(os.environ['NC2ZARR_OUTPUT'], os.environ['NC2ZARR_PUBLIC_URL'] + 'output')
+
     response = requests.put(
         'http://' + domain + '/complete-conversion/intake-source/' + str(complete_conversion_id),
-        data={'yaml': source.yaml()})
+        data={'yaml': yaml})
     response.raise_for_status()
 
 
-def generate_intake_catalog_for_json_metadata(relative_input_path: str, file_name: str, json_workflow_id: int):
-    relative_input_path = build_relative_input_path(relative_input_path)
+def generate_intake_catalog_for_json_metadata(absolute_input_path: str, file_name: str, json_workflow_id: int):
+    absolute_input_path = build_input_path(absolute_input_path)
 
     if not file_name.endswith('.json'):
         file_name = file_name + '.json'
 
-    path = os.path.join(relative_input_path, file_name)
+    path = os.path.join(absolute_input_path, file_name)
 
     source = intake.open_zarr(
         "reference://",
@@ -45,18 +47,16 @@ def generate_intake_catalog_for_json_metadata(relative_input_path: str, file_nam
     if os.environ["NC2ZARR_PROD"] == "True":
         domain = "django:8000"
 
+    yaml = source.yaml().replace(os.environ['NC2ZARR_OUTPUT'], os.environ['NC2ZARR_PUBLIC_URL'] + 'output')
+
     response = requests.put('http://' + domain + '/json-workflow/intake-source/' + str(json_workflow_id),
-                            data={'yaml': source.yaml()})
+                            data={'yaml': yaml})
     response.raise_for_status()
 
 
-def build_relative_intake_catalog_path(path: str) -> str:
-    return build_relative_path(path, 'intake-catalogs')
+def build_input_path(path: str) -> str:
+    return build_absolute_path(os.environ['NC2ZARR_OUTPUT'], path)
 
 
-def build_relative_input_path(path: str) -> str:
-    return build_relative_path(path, 'output')
-
-
-def build_relative_path(path: str, folder_name: str) -> str:
-    return os.path.join("..", folder_name, path).replace("\\", "/")
+def build_absolute_path(prefix: str, suffix: str) -> str:
+    return os.path.join(prefix, suffix).replace("\\", "/")
